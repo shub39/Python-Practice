f00e2dcd8de51671fb9a8aab2a186a1d15a0cdda
Rupsa Chakraborty <ivychakraborty428@gmail.com>
2024-08-20 00:16:40 +0530

    day2 second ques

---
+++ PCC-DS-391/Day_2/2.py
@@ -0,0 +1,11 @@
+#Write a program to accept a number from a user and calculate the sum of all numbers from 1 to a given number
+a = int(input("Enter a number: "))
+s= 0
+for i in range(a+1):
+  s+=i
+print(s)
+
+Output:
+Enter a number: 7
+28
+  
