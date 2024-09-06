014157c5efba190a80b8e976b5977c09cc40bae7
Harsh <harshkr4227@gmail.com>
2024-09-06 21:29:28 +0530

    Easy

---
+++ PCC-DS-391/Day-5/21.py
@@ -0,0 +1,7 @@
+# Write a Python program to check if value 200 exists in the following dictionary.
+
+sample_dict = {'a': 100, 'b': 200, 'c': 300}
+if 200 in sample_dict.values():
+    print("200 is present in the dictionary")
+else:
+    print("200 is not present in the dictionary")
---
+++ PCC-DS-391/Day-5/23.py
@@ -0,0 +1,8 @@
+# Print the key of a minimum value from the following dictionary.
+
+sample_dict = {
+    'Physics' : 82,
+    'Math' : 65,
+    'history' : 75
+}
+print(min(sample_dict.values()))
