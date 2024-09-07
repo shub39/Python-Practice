4bcb9baaa621d9abcefe175decbd4c36dbb661b0
Harsh <harshkr4227@gmail.com>
2024-09-08 01:07:57 +0530

    harsh

--- PCC-DS-391/Day-3/13.py
+++ PCC-DS-391/Day-3/13.py
@@ -3,5 +3,15 @@
 # 6 5 4
 # 10 9 8 7
 
-for i in range(1,5):
-    print(*range(i,0,1))
+start = 1
+stop = 2
+current_Number = stop
+for i in range(2, 6):
+    for j in range(start, stop):
+      current_Number -= 1
+      print(current_Number, end=' ')
+    print("")
+    
+    start = stop
+    stop += i
+    current_Number = stop
